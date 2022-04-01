056723ccc5e6b7b87135bc6727bd5b65f83f5f29
Julian V <jvesper95@gmail.com>
2022-04-01 14:22:38 +0200

    Add crash context parsing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    num::ParseIntError,
+    str::FromStr,
 };
 
 use anyhow::Context;
 use clap::Parser;
 
+#[derive(Debug)]
 struct CrashContext {
     count: usize,
     pc: usize,
     lr: usize,
     paths: Vec<String>,
 }
+
+impl FromStr for CrashContext {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.splitn(4, ' ').collect();
+
+        let count = parts[0].parse::<usize>()?;
+        let pc = usize::from_str_radix(parts[1].strip_prefix("0x").unwrap(), 16)?;
+        let lr = usize::from_str_radix(parts[2].strip_prefix("0x").unwrap(), 16)?;
+        let paths = parts[3].split(' ').map(str::to_string).collect();
+
+        Ok(CrashContext {
+            count,
+            pc,
+            lr,
+            paths,
+        })
+    }
+}
+
+// impl fmt::Display for CrashContext {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "{")
+//     }
+// }
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -28,8 +57,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_context(|| format!("Could not read file {:?}", &args.input_file))?;
     let cc_file_reader = BufReader::new(cc_file);
 
-    for line in cc_file_reader.lines() {
-        println!("{}", line?);
+    let mut crash_contexts: Vec<CrashContext> = Vec::new();
+
+    for line in cc_file_reader.lines().skip(1) {
+        crash_contexts.push(CrashContext::from_str(&line?)?);
+    }
+
+    for crash_context in &crash_contexts {
+        println!("{:?}", crash_context);
     }
 
     Ok(())
